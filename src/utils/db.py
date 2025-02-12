0da45fc2b225aef595fd339f6432eacb2d48dd64
Erika <adob.e@hotmail.com>
2025-02-12 12:21:02 +0100

    feat: db utils commands

--- src/utils/db.py
+++ src/utils/db.py
@@ -2,16 +2,33 @@ import mysql.connector
 import os
 import json
 from dotenv import load_dotenv
+from mysql.connector import errorcode
 
 load_dotenv() 
 config = json.loads(os.getenv("DB_CONFIG")) 
-# Creating connection object
-mydb = mysql.connector.connect(**config)
 
-cursor = mydb.cursor()
 
-# Show database
-cursor.execute("SHOW DATABASES")
+def connexion() :
+    try : 
+        # Creating connection object
+        cnx = mysql.connector.connect(**config)
 
-for x in cursor:
-    print(x)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        return cnx
+    
+def deconnexion(cnx):
+    cnx.close()
+
+def get_cursor(cnx):
+    return cnx.cursor()
+
+def close_cursor(cursor):
+    cursor.close()
+
