2304ab73ef06c38bbc2a19dc32777571f441ec3b
Erika <adob.e@hotmail.com>
2025-05-20 10:50:31 +0200

    fix: update user message rule for personality response and ensure conversation history reset on persona switch

--- src/aclient.py
+++ src/aclient.py
@@ -74,7 +74,7 @@ class discordClient(discord.Client):
 
     async def send_message(self, message, user_message):
         author = message.user.id if self.is_replying_all == "False" else message.author.id
-        user_message_rule = user_message + "Ne fais pas référence à ce message dans ta réponse et suis cette règle : N'oublie pas de répondre avec ta personnalité actuelle et exagère là pour qu'on puisse la reconnaître dans ta réponse."
+        user_message_rule = user_message + "Ne fais pas référence à ce message dans ta réponse et suis cette règle : N'oublie pas de répondre avec ta personnalité actuelle SAUF si l'utilisateur t'a dit de l'oublier et de changer de personnalité et exagère ta personnalité (du jour ou choisie par l'utilisateur) pour qu'on puisse la reconnaître dans ta réponse."
         try:
             response = await self.handle_response(user_message_rule)
             response_content = f'> **{user_message}** - <@{str(author)}> \n\n{response}'
@@ -143,7 +143,7 @@ class discordClient(discord.Client):
 
     async def switch_persona(self, persona) -> None:
         """Change la personnalité et réinitialise l'historique si nécessaire."""
-        # self.reset_conversation_history(force_reset=True)
+        self.reset_conversation_history(force_reset=True)
         persona_prompt = personas.PERSONAS.get(persona)
         await self.handle_response(persona_prompt)
         # await self.send_start_prompt()
@@ -151,8 +151,6 @@ class discordClient(discord.Client):
     async def handle_response(self, user_message) -> str:
         """Gère la réponse en utilisant l'historique de conversation."""
         self.conversation_history.append({'role': 'user', 'content': user_message})
-        if len(self.conversation_history) > 26:
-            del self.conversation_history[4:6]  # Réduire l'historique sans le vider complètement
 
         if os.getenv("OPENAI_ENABLED") == "False":
             async_create = sync_to_async(self.chatBot.chat.completions.create, thread_sensitive=True)
